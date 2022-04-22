b69937dac8dc7ad6cbe15ae3badbef047506ba51
jimin2n7 <nguyenchiminhnt85@gmail.com>
2022-04-23 01:30:56 +0700

    deploy1

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -7,7 +7,7 @@ function CardItem(props) {
         <li className='cards__item'>
             <Link className="cards__item__link" to='/'>
                 <figure className='cards__item__pic-wrap' data-category={props.label}>
-                    <img src={props.src} alt="Travel Image" className='cards__item__img'></img>
+                    <img src={process.env.PUBLIC_URL +props.src} alt="Travel Image" className='cards__item__img'></img>
                 </figure>
                 <div className="cards__item__info">
                     <h5 className="cards__item__text">{props.text}</h5>
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,7 +6,7 @@ import Button from './Button'
 function HeroSection() {
   return (
     <div className='hero-container'>
-        <video src="./videos/video-1.mp4" autoPlay muted></video>
+        <video src={process.env.PUBLIC_URL + "/videos/video-1.mp4"} autoPlay muted></video>
         <h1>ADVENTURE AWAITS</h1>
         <p>What are you waiting for?</p>
         <div className='hero-btns'>
